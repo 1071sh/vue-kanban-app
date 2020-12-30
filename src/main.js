22003c1ad9bfb7e2e8a9770780c1005d0664b0aa
1071sh <k.nishida@viatage-llc.com>
2020-12-30 15:57:28 +0900

    実装完了

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import "es6-promise/auto";
+import "es6-promise/auto"; // プロミスをポリフィルする
 import App from "./App";
+import ErrorBoundary from "./ErrorBoundary.vue"; // エラーを捕捉するコンポーネント
 import router from "./router";
-import store from "./store";
+import store from "./store"; // Vuexのストアインスタンスをインポート
 
 Vue.config.productionTip = false;
+Vue.config.performance = true;
+
+// ErrorBoundaryコンポーネントのインストール
+Vue.component(ErrorBoundary.name, ErrorBoundary);
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error("errorHandler err:", err);
+  console.error("errorHandler vm:", vm);
+  console.error("errorHandler info:", info);
+};
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
-  store,
+  store, // インポートしたストアインスタンスを`store`オプションとして指定
   components: { App },
   template: "<App/>"
 });
